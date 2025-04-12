c4e3b48755bd45c537a55a3528fcc3a15957dd9d
munaibrahimn <munaa97@gmail.com>
2025-04-12 15:24:13 +0100

    README updated

--- challenge/app.py
+++ challenge/app.py
@@ -7,10 +7,15 @@ r = redis.Redis(host='redis', port=6379, db=0)
 
 @app.route('/')
 def hello_world():
-    # Increment the visit count in redis
+    return f'Welcome to my page!'
+
+@app.route('/count')
+def count():
+# Increment the visit count in redis
     r.incr('visit_count')
     visit_count = r.get('visit_count').decode('utf-8')
-    return f'Welcome to my page! You have now been here {visit_count} times!'
+    return f'You have now been here {visit_count} times!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002)
